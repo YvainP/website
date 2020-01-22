cb551a7f4ba169eb57506f842a7a90f770f73c3f
nano <i20-10@hotmail.fr>
2020-01-22 14:05:33 +0100

    toutes les routes done

--- src/public/index.php
+++ src/public/index.php
@@ -14,8 +14,27 @@ $app = new \Slim\App($container);
 //Routes
 //Accueil
 $app->get('/', function (Request $request, Response $response){
-    return $this->view->render($response, 'base.html.twig');
-});
+    return $this->view->render($response, 'home.html.twig');
+})->setName('home');
+
+//Cv
+$app->get('/myCV', function (Request $request, Response $response){
+    return $this->view->render($response, 'cv.html.twig');
+})->setName('cv');
+
+//my projects
+$app->get('/myProjects', function (Request $request, Response $response){
+    return $this->view->render($response, 'projects.html.twig');
+})->setName('projects');
+//profile
+$app->get('/myProfile', function (Request $request, Response $response){
+    return $this->view->render($response, 'profile.html.twig');
+})->setName('profile');
+
+//contact
+$app->get('/contact', function (Request $request, Response $response){
+    return $this->view->render($response, 'contact.html.twig');
+})->setName('contact');
 
 //lancement de l'appli
 $app->run();
